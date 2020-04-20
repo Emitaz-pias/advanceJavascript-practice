eb753a72ad4434cf9bdde7d24bc1b846785a8932
Emitaz-pias <61836021+Emitaz-pias@users.noreply.github.com>
2020-04-20 11:05:57 +0600

    ==vs===done

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,8 @@
+const num1 =1;
+const num2 =true;
+if (num1==num2) {
+    console.log("codition is true");
+} 
+else {console.log("condition is false");
+}
+//note:==checks only the values...===checks the data types too..
