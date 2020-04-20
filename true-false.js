fcfada3475bf0262ae6055e6dd07960ac7b0704c
Emitaz-pias <61836021+Emitaz-pias@users.noreply.github.com>
2020-04-20 10:22:22 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,16 @@
+//falsy:
+//""
+//0
+//undefined
+//null
+//NaN
+//false
+//truthy..:
+//"0 " " " 1 "name" [] {}
+const name = "Neetu"
+if (name) {
+    console.log("condition is true")
+    
+} else {
+    console.log("condition is false")
+}
